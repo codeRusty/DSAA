7cd9227e766f872b13a55ecbc2407aa036b91522
codeRusty <sourabh.rustagi@hotmail.com>
2018-11-26 07:34:04 +0530

    New Problems added

---
+++ Problems/CuttingRod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackToSuccess.Problems
+{
+    public class CuttingRod
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("Please provide the length of the rod in INT!");
+            int rodLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] priceArray = new int[] { 12, 23, 34, 45, 46, 56, 67, 78, 84, 85, 92 };
+
+            int[] totalCost = new int[priceArray.Length];
+
+            for (int i = 0; i < rodLength; i++) {
+                int currCost = priceArray[i] * rodLength;
+                totalCost[i] = currCost;
+            }
+
+        }
+    }
+}
---
+++ Problems/Palindrome.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackToSuccess.Problems
+{
+    public class Palindrome
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("Enter the string to find the Largest Palindrome");
+            string sample = Console.ReadLine();
+            string palindrome = GetLargestPalindrome(sample);
+            Console.WriteLine("found :" + palindrome);
+
+        }
+
+        private static string GetLargestPalindrome(string test)
+        {
+            int strLength = test.Length;
+
+            List<string> holder = new List<string>();
+            int maxYet = 0;
+            for (int i = 0; i < strLength; i++)
+            {
+                for (int j = strLength - i; j > 1; j--)
+                {
+                    string toTest = test.Substring(i, j);
+                    if (maxYet < toTest.Length)
+                        continue;
+                    if (IsPalindrome(toTest))
+                    {
+                        maxYet = toTest.Length;
+                        holder.Add(toTest);
+                    }
+                }
+            }
+
+            return holder.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur);
+        }
+
+        private static bool IsPalindrome(string str)
+        {
+            char[] a = str.ToCharArray();
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return a.SequenceEqual(charArray);
+        }
+    }
+}
